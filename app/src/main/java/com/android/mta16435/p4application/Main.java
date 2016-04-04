4fced8b9a4b1c7726cb2b3f22fc08aca4ded455d
Runagar <runagar@gmail.com>
2016-04-04 12:47:44 +0200

    Base app test

--- app/src/main/java/com/android/mta16435/p4application/Main.java
+++ app/src/main/java/com/android/mta16435/p4application/Main.java
@@ -1,7 +1,9 @@
 package com.android.mta16435.p4application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToBatman(View view){
+        Intent intent = new Intent(this, TestComicView.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/android/mta16435/p4application/TestComicView.java
@@ -0,0 +1,13 @@
+package com.android.mta16435.p4application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestComicView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_comic_view);
+    }
+}
